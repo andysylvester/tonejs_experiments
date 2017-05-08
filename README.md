eac559d9b3fa5f42a5e5d29a0f71a49445859709
Andy Sylvester <andy@andysylvester.com>
2017-05-07 22:49:04 -0700

    Initial app commit
    Addition of initial app, license file and README file

---
+++ README.md
@@ -0,0 +1,23 @@
+README.md
+=========
+
+Overview
+--------
+
+This folder contains files exploring using the Tone.js and NexusUI Javascript libraries to implement the musical theories of the composer Jospeh Schillinger.
+
+index.html - Single page app with a "Play" button. Calculates a Schiilinger resultant (see my <a href="https://www.perl.com/pub/2007/08/09/making-perl-modules.html">article</a> on Perl.com for a better description of the concept).
+
+You can see/hear the app at http://andysylvester.com/files/tonejs_experiments/
+
+Credits
+-------
+
+https://github.com/Tonejs/Tone.js - Tone.js Github page
+
+https://github.com/lsu-emdm/nexusUI/ - NexusUI Github page
+
+http://joeldarling.com/joliesynth/ - Used this as a basis for the app
+
+
+
---
+++ js/app.js
@@ -0,0 +1,101 @@
+
+/// SETUP OSC ///
+var monoSynth = new Tone.MonoSynth({
+
+    "filterEnvelope" : {
+        "attack" : 0.02,
+        "decay" : 0.1,
+        "sustain" : 0.2,
+        "release" : 0.9,
+    },
+    filter: {
+      type: 'lowpass'
+    },
+    oscillator:{
+      type:"sine"
+    }
+}).toMaster();
+
+var monoSynth = new Tone.MonoSynth().toMaster();
+
+//set initial volume
+Tone.Master.volume.rampTo(-10, 0.05);
+
+nx.onload = function() {
+  nx.colorize("#212121"); // sets accent (default)
+
+  initControls();
+
+  //event listeners
+
+  /// BUTTON ///
+  midiStatus.on("*", function(data){
+    playPattern();
+
+  });
+
+};
+
+var initControls = function(){
+  midiStatus.mode = "impulse";
+};
+
+function resultant() {
+	var major_generator = 5;
+	var minor_generator = 3;
+
+	var total_counts   = major_generator * minor_generator;
+	var result_counter = 0;
+	var major_mod      = 0;
+	var minor_mod      = 0;
+	var i              = 0;
+	var j              = 0;
+	var resultant      = [];
+
+	console.log("Generator Total = %d \n", total_counts);
+
+	while (i < total_counts) {
+	   i++;
+	   result_counter++;
+	   major_mod = i % major_generator;
+	   minor_mod = i % minor_generator;
+	   if ((major_mod == 0) || (minor_mod == 0)) {
+		  resultant.push(result_counter);
+		  result_counter = 0;
+	   }
+	   console.log("%d \n", i);
+	   console.log("Modulus of %d is %d  \n", major_generator, major_mod);
+	   console.log("Modulus of %d is %d  \n", minor_generator, minor_mod);
+	}
+
+	console.log("\n");
+	console.log("The resultant is ", resultant);
+	console.log("The resultant length is ", resultant.length);
+	return resultant;
+};
+
+var playPattern = function(){
+
+    var test = resultant();
+    var listOfObjects = [];
+    var i = 0;
+    // Create an array of events
+	test.forEach(function(entry) {
+		var singleObj = {};
+		singleObj['time'] = i;
+		singleObj['note'] = Tone.Frequency(entry + 60, "midi");
+		singleObj['dur'] = 1;
+		listOfObjects.push(singleObj);
+		i++;
+	});
+
+	//pass in an array of events
+	var part = new Tone.Part(function(time, event){
+		//the events will be given to the callback with the time they occur
+		monoSynth.triggerAttackRelease(event.note, event.dur, time)
+	}, listOfObjects);
+	//start the part at the beginning of the Transport's timeline
+	part.start(0);
+    Tone.Transport.start('+0.1');
+
+}
